e1da3c42578e01f64ce28544be2f5bd436d86bc1
ffphoenix <freeflyingphoenix@gmail.com>
2026-01-21 22:01:14 +0100

    Add `useHistorySocket` hook for game history subscription
    - Implement socket connection for game history updates, subscribing and unsubscribing to game events dynamically.
    - Add message handling with `GameHistoryMessages` MobX store.

---
+++ frontend/src/pages/gameScene/gameMessagesHistory/components/useHistorySocket.ts
@@ -0,0 +1,24 @@
+import socketManager from "../../../../utils/socketManager";
+import { useEffect } from "react";
+import gameStore from "../../store/GameStore";
+import gameHistoryMessages from "../store/GameHistoryMessages";
+
+const useHistorySocket = () => {
+  const socket = socketManager.socket("/game-history");
+
+  useEffect(() => {
+    socket.connect();
+    socket.emit("subscribeToGame", gameStore.game?.id);
+
+    socket.on("historyItemCreated", (data) => {
+      console.log(data);
+      gameHistoryMessages.addUserMessage(data.userId, data.body.message);
+    });
+
+    return () => {
+      socket.emit("unsubscribeFromGame", gameStore.game?.id);
+      socket.disconnect();
+    };
+  }, []);
+};
+export default useHistorySocket;
